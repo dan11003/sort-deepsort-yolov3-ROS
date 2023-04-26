fd1edbd8d5445906e3d5a719f740c24780be52fd
daniel adolfsson <dla.adolfsson@gmail.com>
2023-04-26 17:51:09 +0200

    added plot

--- sort_track/src/track.py
+++ sort_track/src/track.py
@@ -63,18 +63,36 @@ class MultiObjectTracker:
 
     def SyncedCallback(self,detectMsg, imageMsg):
 
-        listDetections = []
+        print("callback")
+        #listDetections = []
+        #listDetections = [list(np.array()) for i in range(CLASSES)]
+        listDetections = dict()
+
+        #for i in CLASSES.keys():
+        #    listDetections[i] = list(np.array([]))
+
+        listDetections[i] = [list(np.array([])) for i in CLASSES.keys()]
+        print(listDetections)
+
+
+
+        print(type(listDetections))
+        print(listDetections)
         for detection in detectMsg.detections:
             bbox = detection.bbox
+            label = int(detection.results[0].id)
             confidence = round(float(100*detection.results[0].score),2)
-            listDetections.append(np.array([int(bbox.center.x-bbox.size_x), int(bbox.center.y-bbox.size_y), int(bbox.center.x+bbox.size_x), int(bbox.center.y+bbox.size_y), confidence, detection.results[0].id]))
+            print("at: " + str(label))
+            npDetection = np.array([int(bbox.center.x-bbox.size_x), int(bbox.center.y-bbox.size_y), int(bbox.center.x+bbox.size_x), int(bbox.center.y+bbox.size_y), confidence, label])
+            listDetections[label].append(npDetection)
 
 
         #scores_new = np.array([d.confidence for d in detections_new])
         #indices = prep.non_max_suppression(boxes, 1.0 , scores_new)
         #detections_new = [detections_new[i] for i in indices]
-
+        detections = {}
         detections = np.array(listDetections)
+
         if(detections.shape[0] > 0):
             boxes = detections[:,0:4]#np.array([d.tlwh for d in detections])
             confidence = detections[:,4]
@@ -109,7 +127,7 @@ class MultiObjectTracker:
         if self.vis_tracked_objects:
             for i in range(self.track.shape[0]):
                 cv2.rectangle(cv_rgb, (self.track[i][0], self.track[i][1]), (self.track[i][2], self.track[i][3]), (255, 255, 255), 1)
-                cv2.putText(cv_rgb , str(self.track[i][4]), (self.track[i][2], self.track[i][1]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), lineType=cv2.LINE_AA)
+                cv2.putText(cv_rgb , str(self.track[i][4]), (self.track[i][2], self.track[i][3]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), lineType=cv2.LINE_AA)
         cv2.imshow("YOLO+SORT", cv_rgb)
         cv2.waitKey(3)
 
@@ -125,9 +143,16 @@ def main():
     print("Initialize ROS node")
     rospy.init_node('sort_tracker', anonymous=False)
     rate = rospy.Rate(10)
-    #rosTracker = [MultiObjectTracker() for i in range(CLASSES)]
+    rosTracker = {}
+    for i in CLASSES.keys():
+        rosTracker[i] = MultiObjectTracker()
 
-    rosTracker = MultiObjectTracker()
+    #print(type(rosTracker))
+    #print(rosTracker)
+
+
+    #rosTracker = MultiObjectTracker()
+    print("Initialize finished")
     while not rospy.is_shutdown():
         rate.sleep()
         rospy.spin()
